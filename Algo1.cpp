a94b3f79f643ff7d9f3f31dab86dd6018e6646fc
Shubham Kumar Singh <shubham12an@gmail.com>
2026-02-01 00:15:03 +0530

    This is my Day 1 in DSA journey

---
+++ Algo1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void swap_Algorithm(int a, int b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+
+  cout << a;
+  cout << b;
+}
+
+int main()
+{
+  //  g++ Algo1.cpp -o Algo1 ; .\Algo1
+
+  // algorithms are written at Design tym and then implementation
+  // thre person who writes the algorithm must have good domain knowledege
+  // as algorithms are developed on designed tym it can be written in english then programmer made that program after designing
+
+  // algorithms must have these things
+  // input 0 or more , output atleast 1  , definitness , finitiness , effectiveness
+
+  // Criteria to analyze an algorithm
+  //  1. Time , 2. Space , 3.
+
+  // cout << "Hello World";
+  cout << "New to algorithms here" << endl;
+
+  swap_Algorithm(4, 5);
+}
